4c5ea4e0ae83b00a22f6509e717ec103549f816d
vitorhipolito <120065778+vitorhipolito@users.noreply.github.com>
2022-12-12 20:40:26 -0300

    Add files via upload

---
+++ ssssss.py
@@ -0,0 +1,12 @@
+#Nome:Vitor hipolito coelho de oliveira
+#Idade:17
+#Turma: 2N sistemas
+
+
+val1 = 5
+val2 = 0.6
+val3 = 1/2
+val4 = 9.81
+val5 = 0.6 ** 2
+
+print(val1 * val2 - val3 * val4 * val5)
